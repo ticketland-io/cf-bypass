041d37a92e58ec1d6349e6a1eaf597911d6ebaef
ppoliani <ppoliani@gmail.com>
2023-05-12 18:58:34 +0300

    chore: test multiple client requests

--- file-downloader/src/main.rs
+++ file-downloader/src/main.rs
@@ -1,18 +1,19 @@
 use std::{fs::File, io::Write};
-
 use eyre::Result;
+use futures::future::join_all;
 use file_downloader::{
   grpc_client::{GrpcClient, file_server::FileResponse},
   image::{resize_img, svg_to_png},
 };
 
-#[tokio::main]
-async fn main() -> Result<()> {
-  for _ in 0..1 {
-    // let uri = "https://nft.blocto.app/aptos-blocto-lfb/blocto.png".to_string();
+async fn run(id: usize) -> Result<()> {
+  println!("Running task {id}");
+
+  for _ in 0..10 {
+    let uri = "https://nft.blocto.app/aptos-blocto-lfb/blocto.png".to_string();
     // let uri = "https://art-sandbox.sunflower.industries/token/0xd58434f33a20661f186ff67626ea6bdf41b80bca/955".to_string();
     // let uri = "https://cloudflare-ipfs.com/ipfs/QmVet4B2giUTyyJ9rAkU8oRR8TcL5XRkdjcm6TVDaKjSHy".to_string();
-    let uri = "https://gateway.pinata.cloud/ipfs/QmXiSJPXJ8mf9LHijv6xFH1AtGef4h8v5VPEKZgjR4nzvM".to_string();
+    // let uri = "https://gateway.pinata.cloud/ipfs/QmXiSJPXJ8mf9LHijv6xFH1AtGef4h8v5VPEKZgjR4nzvM".to_string();
     let mut client = GrpcClient::new("http://[::1]:50051").await?;
     let FileResponse {mime_type, data} = client.download_file(uri).await?;
 
@@ -30,7 +31,22 @@ async fn main() -> Result<()> {
 
     let mut file = File::create("image.png")?;
     file.write_all(&image_data)?;
+
+    println!("Processed task {id}");
   }
+
+  Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+  let mut tasks = Vec::with_capacity(10);
+
+  for id in 0..10 {
+    tasks.push(tokio::spawn(run(id)));
+  }
+
+  join_all(tasks).await;
   
   Ok(())
 }
